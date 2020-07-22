8f0f8d726738343df5d3ddecb03749f9e0d6d252
Reinhard Herzog <reinhard.herzog@iosb.fraunhofer.de>
2020-07-22 17:01:45 +0200

    use same package names for tests

--- TS_HelloWorld/src/test/java/de/fraunhofer/iosb/tc_lib_helloworld/CountryValuesTest.java
+++ TS_HelloWorld/src/test/java/de/fraunhofer/iosb/tc_lib_helloworld/CountryValuesTest.java
@@ -1,29 +1,24 @@
-package de.fraunhofer.iosb.tc_lib_helloworld_Tests;
+package de.fraunhofer.iosb.tc_lib_helloworld;
 
 
-// import org.junit.Test;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.fail;
 
+import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import de.fraunhofer.iosb.tc_lib_helloworld.CountryValues;
 
 class CountryValuesTest {
 
 	final Logger log = LoggerFactory.getLogger(CountryValuesTest.class);
-
 
 	@Test
 	void test() {
 		CountryValues cv = new CountryValues("TestCountry");
 		cv.setPopulation(1.0f);
 		cv.setPopulation(1.01f);
-		assertTrue(cv.testPopulation(0.01f, log));
-		assertFalse(cv.testPopulation(1.01f, log));
+		assertTrue("Assert to discover wrong rate", cv.testPopulation(.01f, log));
+		assertFalse("Assert to acknowledge correct rate", cv.testPopulation(1.01f, log));
 	}
 
 }
