81aed21e91cd804643f7f37dace0074ab25d7095
Reinhard Herzog <reinhard.herzog@iosb.fraunhofer.de>
2021-07-14 14:49:07 +0200

    cleanup assert condition

--- TS_HelloWorld/src/test/java/de/fraunhofer/iosb/tc_helloworld/TC0001Test.java
+++ TS_HelloWorld/src/test/java/de/fraunhofer/iosb/tc_helloworld/TC0001Test.java
@@ -1,6 +1,6 @@
 package de.fraunhofer.iosb.tc_helloworld;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertSame;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
@@ -17,6 +17,6 @@ class TC0001Test extends TSTest {
 		setUp(testCase);
 		IVCT_Verdict verdict = testCase.execute(tcParamJson, runLogger);
 		runLogger.info("Test Case Verdict: {}", verdict);
-		assertTrue(verdict.verdict == IVCT_Verdict.Verdict.PASSED);		
+		assertSame("Test Case shall pass", IVCT_Verdict.Verdict.PASSED, verdict.verdict);		
 	}
 }
